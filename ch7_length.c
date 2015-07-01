5ada4fb581ca9004f40b2c222fa2ee5c7f06ca0d
naverlatte <naverlatte@gmail.com>
2015-07-01 14:03:04 +0200

    init. determines the length of a message

---
+++ ch7_length.c
@@ -0,0 +1,20 @@
+//Name: ch7_length.c
+//Purpose: Determines the length of a message.
+
+#include <stdio.h>
+int main (void)
+{
+	char ch;
+	int len=0;
+	
+	printf("Enter a message: ");
+	ch=getchar();
+	while (ch!='\n'){
+		len++;
+		ch=getchar();
+	}
+	printf("Your message was %d character(s) long.\n",len);
+
+	return 0;
+}
+
